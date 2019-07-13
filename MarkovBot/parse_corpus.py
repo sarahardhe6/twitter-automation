bbb742a27a26af1b9055200a61a13bd9b350d4b5
moonero <eth2moon@gmail.com>
2019-07-12 19:11:31 -0700

    Added python script to parse corpus and replace keywords with new words

---
+++ MarkovBot/parse_corpus.py
@@ -0,0 +1,16 @@
+newlines = []
+wordlist = ['McDonalds','McMuffin','egg McMuffin','mcdonalds','mcds']
+
+with open('mcds.txt') as file:
+    text = file.readlines()
+    print(text)
+
+    for line in text:
+        for word in line:
+            if word in {'McDonalds','mcdonalds', 'mcds'}
+                newstring = str.replace(line, word, 'Republican')
+                print(newstring)
+
+
+
+

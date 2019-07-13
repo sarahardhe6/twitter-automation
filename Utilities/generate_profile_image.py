1a217f027f3b375c79425088eb81af3a3440eb85
moonero <eth2moon@gmail.com>
2019-07-12 19:04:25 -0700

    Added python script to generate profile pics (using blockies) from a string

---
+++ Utilities/generate_profile_image.py
@@ -0,0 +1,51 @@
+import blockies
+
+data = blockies.create('moonero1')
+name = 'moonero1.png'
+with open(name, 'wb') as png:
+    png.write(data)
+
+data = blockies.create('moonero2')
+name = 'moonero2.png'
+with open(name, 'wb') as png:
+    png.write(data)
+
+data = blockies.create('moonero3')
+name = 'moonero3.png'
+with open(name, 'wb') as png:
+    png.write(data)
+
+data = blockies.create('moonero4')
+name = 'moonero4.png'
+with open(name, 'wb') as png:
+    png.write(data)
+
+data = blockies.create('moonero5')
+name = 'moonero5.png'
+with open(name, 'wb') as png:
+    png.write(data)
+
+data = blockies.create('moonero6')
+name = 'moonero6.png'
+with open(name, 'wb') as png:
+    png.write(data)
+
+data = blockies.create('moonero7')
+name = 'moonero7.png'
+with open(name, 'wb') as png:
+    png.write(data)
+
+data = blockies.create('moonero8')
+name = 'moonero8.png'
+with open(name, 'wb') as png:
+    png.write(data)
+
+data = blockies.create('moonero9')
+name = 'moonero9.png'
+with open(name, 'wb') as png:
+    png.write(data)
+
+data = blockies.create('moonero10')
+name = 'moonero10.png'
+with open(name, 'wb') as png:
+    png.write(data)
